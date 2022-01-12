5774aa16c2bbbd566b693d1cfaf0213864818e4c
Phil <peburney@ncsu.edu>
2022-01-12 17:37:12 -0500

    Added static image rendering and allow users to view different images

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -16,4 +16,5 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 0,
   }
+
 }
--- front-end/src/components/UI/ListingBox.vue
+++ front-end/src/components/UI/ListingBox.vue
@@ -3,21 +3,46 @@
     <div class="row">
       <h2 class="p-2 text-center title">{{ listing.title }}</h2>
     </div>
-    <div class="row ">
-      <div class="col-md-4 col-lg-4 d-flex justify-content-center">
-      <div class="" id="imgcontainer">
-        <img
-          v-if="listing.images[0]"
-          id="image"
-          class="p-2 img-fluid"
-          :src="currentPhoto"
-        />
+    <div class="row">
+      <div class="col-md-4 col-lg-4 d-flex flex-row justify-content-center">
+        <div v-if="listing.images.length > 0" class="d-flex flex-column justify-content-center" id="imgcontainer" :style = "{ backgroundImage: 'url(' + listing.images[imgIdx] + ')' }">
+          <div class="d-flex flex-grow-1 align-items-end">
+          <div >
+          <img
+            id="leftarrow"
+            src="../../assets/arrow.svg"
+            class="img-fluid m-2 p-1"
+            v-on:click="decrementIdx"
+          />
+          </div>
+          <div class="w-100 d-flex justify-content-end">
+          <img
+            id="rightarrow"
+            src="../../assets/arrow.svg"
+            class="img-fluid m-2 p-1"
+            v-on:click="incrementIdx"
+          />
+          </div>
+          </div>
+          <div  class = "flex-grow-1 d-flex align-items-end justify-content-center">
 
-        <img v-else id="image" src="../../assets/part.svg" class="img-fluid" />
+            <div id= "imgcounter" class = "p-1">
+            Image {{imgIdx + 1}} of {{ listing.images.length }}
+            </div>
+          </div>
         </div>
+        <div v-else-if="isPartListing" id="imgcontainer" >
+          <img src="../../assets/part.svg" id="defaultimg">
+        </div>
+        <div v-else-if="isCarListing" id="imgcontainer">
+          <img src="../../assets/car.svg" id="defaultimg">
+        </div>
+        
       </div>
       <div class="col-md-4 col-lg-3 p-3" id="basicinfo">
-        <h2 class="text-nowrap" v-if="isBountyListing">Bounty: ${{ listing.bounty }}</h2>
+        <h2 class="text-nowrap" v-if="isBountyListing">
+          Bounty: ${{ listing.bounty }}
+        </h2>
         <h2 v-else>Sale Price: ${{ listing.salePrice }}</h2>
 
         <table>
@@ -40,12 +65,11 @@
         </table>
       </div>
       <div class="col-md-4 col-lg-5 p-2" id="advinfo">
-          <div class=" descriptionbox d-flex flex-column">
-            <h4 class="text-center">Description</h4>
-            <div class="p-2">{{ listing.description }}</div>
-          </div>
+        <div class="descriptionbox d-flex flex-column">
+          <h4 class="text-center">Description</h4>
+          <div class="p-2">{{ listing.description }}</div>
+        </div>
         <h5 class="pt-3">Email: {{ listing.email }}</h5>
-
       </div>
     </div>
   </div>
@@ -72,26 +96,31 @@ export default class ListingBox extends ListingProps {
     super();
   }
   seeMore() {
-    console.log("")
-  }
-  created() {
-    if (this.listing.images[0]) {
-      this.currentPhoto = this.listing.images[0];
-      console.log(this.currentPhoto)
-    }
+    console.log("");
   }
   data() {
     return {
       photoArray: [],
       currentPhoto: undefined,
+      imgIdx: 0
     };
+  }
+  incrementIdx() {
+    if(this.imgIdx < this.listing.images.length - 1) {
+      this.imgIdx++
+    }
+  }
+  decrementIdx() {
+    if(this.imgIdx >= 1) {
+      this.imgIdx--
+    }
   }
 }
 </script>
 
 <style scoped>
 div {
-  color: black
+  color: black;
 }
 div [class*="sale-box"] {
   border: grey solid 1px;
@@ -113,5 +142,33 @@ div [class*="descriptionbox"] {
   height: 300px;
   width: 350px;
   border: solid grey 1px;
+  background-size: cover;
+  background-position: center;
+}
+#image {
+  width: inherit;
+  height: inherit;
+  
+}
+#leftarrow {
+  height: 50px;
+  fill:white;
+  background: rgba(255,255,255,0.5);
+   border-radius: 5px;
+}
+#rightarrow {
+  height: 50px;
+  transform: scaleX(-1);
+  background: rgba(255,255,255,0.5);
+   border-radius: 5px;
+}
+#imgcounter {
+  background: rgba(255,255,255,0.5);
+   border-radius: 5px;
+}
+#defaultimg {
+  object-fit: cover;
+  height: 300px;
+  width: 350px;
 }
 </style>
